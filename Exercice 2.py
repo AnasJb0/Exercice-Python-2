60d0b3d027ce20d8b4cc9a611b1c72781fb9f000
Anas Jbara <146947467+AnasJb0@users.noreply.github.com>
2023-10-18 12:33:59 +0100

    Add files via upload

---
+++ Exercice 2.py
@@ -0,0 +1,9 @@
+x = input("Enter a character string:")
+while x=="" :
+    print("the character string is empty.")
+    x = input("Please enter a new character string:")
+else:
+    print("the character string is not empty.")
+
+
+
